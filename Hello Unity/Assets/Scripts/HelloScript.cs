9a85ba84bc2d4542abb2d5cc4640c9835318d6a8
DhabiaKM <44834193+DhabiaKM@users.noreply.github.com>
2021-09-11 18:25:12 +0400

    Arab Bonsai Tree Commit 1
    Pot/Sand kinda looking okay

---
+++ Hello Unity/Assets/Scripts/HelloScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Start!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+        transform.position= new Vector3(Mathf.Sin(Time.time)*5.0f,0,3); 
+    }
+}
